02dd64fe7d752346e5570981d144b5277ec9e66f
Maquiz1 <manquiz92@gmail.com>
2022-11-01 16:08:48 +0300

    fix locations2

--- menu.php
+++ menu.php
@@ -91,7 +91,7 @@ if ($user->data()->accessLevel == 1) {
             <a href="#"><span class="isw-archive"></span><span class="text">Receiving</span></a>
             <ul>
                 <li class="">
-                    <a href="add.php?id=13">
+                    <a href="add.php?id=11">
                         <span class="glyphicon glyphicon-plus"></span><span class="text">Add Batch Details</span>
                     </a>
                 </li>
@@ -177,7 +177,7 @@ if ($user->data()->accessLevel == 1) {
             <a href="#"><span class="isw-lock"></span><span class="text">Managements</span></a>
             <ul>
                 <li class="">
-                    <a href="add.php?id=14">
+                    <a href="add.php?id=9">
                         <span class="glyphicon glyphicon-plus"></span><span class="text">Add Generic Name</span>
                     </a>
                 </li>
